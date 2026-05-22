c574598d1192ec6bdb8d5de3b6365b0dbdd7bdcc
Gian Alfonso <alfonsogian225@gmail.com>
2026-05-22 00:05:58 -0500

    Initial Commit
    - Added Prefabs along with their corresponding scripts:
      - Box
      - Grid

---
+++ Assets/Scripts/grid.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    public int sizeX = 10;
+    public int sizeY = 10;
+
+    [Tooltip("The value of \"Size Y\" will be set to \"Size X\" if true.")]
+    public bool isSquare = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.name = "Sol System";
+
+        if (isSquare)
+        {
+            sizeY = sizeX;
+        }
+
+        // Debug
+        Debug.Log("GRID: " + gameObject.name + "\n" +
+            "sizeX = " + sizeX + "\n" +
+            "sizeY = " + sizeY + "\n" +
+            "isSquare = " + isSquare);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
