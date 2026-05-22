1664a6eb20a66368ca5459f21d8feec47847e793
Gian Alfonso <alfonsogian225@gmail.com>
2026-05-22 12:15:47 -0500

    Updated box sprites, spawning, and prefabs
    - Added sprite support and click behavior to box (closed/opened sprites, SpriteRenderer cache, switch to mouse-release and sprite swap on open).
    - Introduce spawnType enum and procedural SpawnBoxes in grid: instantiate box prefab instances, name them, assign traps based on mineDensityPercentage, and track trap/total counts.
    - Update Grid prefab and scene defaults (sizeX/sizeY set to 10, add box reference, mineDensityPercentage and spawnType fields) and adjust SampleScene prefab overrides to reference the box prefab.
    - Also update Box_unclicked image LFS oid and metadata (texture size changed to 100x100).

--- Assets/Scripts/box.cs
+++ Assets/Scripts/box.cs
@@ -6,10 +6,16 @@ public class box : MonoBehaviour
     public bool isTrap = false;
     public bool isOpened = false;
 
+    public Sprite closedSprite;
+    public Sprite openedSprite;
+
+    private SpriteRenderer sr;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = closedSprite;
     }
 
     // Update is called once per frame
@@ -21,7 +27,7 @@ public class box : MonoBehaviour
     void IsBoxedClicked()
     {
         // The box is selected and is not opened yet
-        if (Mouse.current.leftButton.wasPressedThisFrame && !isOpened)
+        if (Mouse.current.leftButton.wasReleasedThisFrame && !isOpened)
         {
             Vector2 worldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
@@ -30,9 +36,11 @@ public class box : MonoBehaviour
             {
                 isOpened = true;
                 if (isTrap)
-                    Debug.Log("You opened a trap box!");
+                    Debug.Log(gameObject.name + " is a trap!");
                 else
-                    Debug.Log("You opened a safe box!");
+                    Debug.Log(gameObject.name + " is a safe box!");
+
+                sr.sprite = openedSprite;
             }
         }
     }
--- Assets/Scripts/grid.cs
+++ Assets/Scripts/grid.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
 
+public enum spawnType
+{
+    BottomLeft  = 0,
+    BottomRight = 1,
+    TopLeft     = 2,
+    TopRight    = 3,
+    Center      = 4
+}
 public class grid : MonoBehaviour
 {
+    public GameObject box;
+
     public int sizeX = 10;
     public int sizeY = 10;
 
@@ -10,6 +20,11 @@ public class grid : MonoBehaviour
 
     [Tooltip("The value of \"Size Y\" will be set to \"Size X\" if true.")]
     public bool isSquare = true;
+
+    public spawnType spawnType = spawnType.BottomLeft;
+
+    private int trapCount = 0;
+    private int totalCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,16 +36,53 @@ public class grid : MonoBehaviour
             sizeY = sizeX;
         }
 
+        SpawnBoxes(spawnType);
+
         // Debug
         Debug.Log("GRID: " + gameObject.name + "\n" +
             "sizeX = " + sizeX + "\n" +
             "sizeY = " + sizeY + "\n" +
-            "isSquare = " + isSquare);
+            "isSquare = " + isSquare + "\n" +
+            "Trap Count = " + trapCount + "/" + totalCount);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnBoxes(spawnType type)
+    {
+        // Calculate offset based on spawn type
+        Vector3 offset = type switch
+        {
+            spawnType.BottomLeft  => new Vector3(0, 0, 0),
+            spawnType.BottomRight => new Vector3(-sizeX, 0, 0),
+            spawnType.TopLeft     => new Vector3(0, -sizeY, 0),
+            spawnType.TopRight    => new Vector3(-sizeX, -sizeY, 0),
+            spawnType.Center      => new Vector3(-sizeX / 2f, -sizeY / 2f, 0),
+            _                     => Vector3.zero
+        };
+
+        // Spawns boxes in a grid pattern with the applied offset
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                Vector3 spawnPos = new Vector3(x, y, 0) + offset;
+
+                // Set name on the instance, not the prefab
+                GameObject instance = Instantiate(box, spawnPos, Quaternion.identity);
+                instance.name = "Box (" + x + ", " + y + ")";
+                // Set trap
+                if (Random.Range(0f, 100f) < mineDensityPercentage)
+                {
+                    instance.GetComponent<box>().isTrap = true;
+                    trapCount++;
+                }
+                totalCount++;
+            }
+        }
     }
 }
